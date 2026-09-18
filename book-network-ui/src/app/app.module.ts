b301c57e7b239f5ddf06fc71fe7b45c453e4201c
Maycem AMOR <maycemamor@gmail.com>
2026-09-18 16:17:11 +0200

    Implement the Registration page

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -6,11 +6,13 @@ import {LoginComponent} from './pages/login/login.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
